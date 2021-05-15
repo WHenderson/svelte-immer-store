8c6921b60dd72c5385f60b6b53fafe3b67a0397f
Will <will.henderso.n@gmail.com>
2021-05-16 00:35:21 +1000

    fix: Simplify tracker proxy

--- src/proxy/tracker.ts
+++ src/proxy/tracker.ts
@@ -1,53 +1,50 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return,@typescript-eslint/ban-types */
-export const symDetails = Symbol('details');
+export const symTrackerDetails = Symbol('details');
 
-export type Tracker<T> = {
-    readonly [symDetails]: {
+export type TrackerProxy<T> = {
+    readonly [symTrackerDetails]: {
         value: T;
         parent: object;
         property: PropertyKey;
     };
 } & (
     T extends object
-    ? { [ P in keyof T]: Tracker<T[P]> }
-    : T
+        ? { [ P in keyof T]: TrackerProxy<T[P]> }
+        : T
 );
 
 export function createTrackerProxy<T>(
     value: T
-) : Tracker<T>;
+) : TrackerProxy<T>;
 
 export function createTrackerProxy<T>(
     value: T,
     parent: object,
     property: PropertyKey
-) : Tracker<T>;
+) : TrackerProxy<T>;
 
 export function createTrackerProxy<T>(
     value: T,
     parent?: object,
     property?: PropertyKey
-) : Tracker<T> {
+) : TrackerProxy<T> {
     const details = { value, parent, property };
 
-    return <Tracker<T>><unknown>new Proxy(
+    return <TrackerProxy<T>><unknown>new Proxy(
         {},
         {
             get(target: never, p: PropertyKey, receiver: never) {
-                if (p === symDetails)
+                if (p === symTrackerDetails)
                     return details;
 
                 const result = (() => {
                     try {
                         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-                        const result: object = Reflect.get(<object><unknown>value, p, receiver);
-                        if (result && typeof result === 'object')
-                            return result;
+                        return <object>Reflect.get(<object><unknown>value, p, receiver);
                     }
                     catch (ex) {
                         return;
                     }
-                    return;
                 })();
 
                 return createTrackerProxy(result, <object><unknown>(value && typeof value === 'object' ? value : undefined), p);
--- tests/proxy/tracker.spec.ts
+++ tests/proxy/tracker.spec.ts
@@ -1,4 +1,4 @@
-import {createTrackerProxy, symDetails} from "../../src/proxy/tracker";
+import {createTrackerProxy, symTrackerDetails} from "../../src/proxy/tracker";
 
 const root = {
     a: 1,
@@ -17,51 +17,50 @@ const root = {
     ]
 };
 
-
 it('track values', () => {
-    expect(createTrackerProxy(root)[symDetails].value).toEqual(root);
-    expect(createTrackerProxy(root).a[symDetails].value).toEqual(root.a);
-    expect(createTrackerProxy(root).b[symDetails].value).toEqual(root.b);
-    expect(createTrackerProxy(root).c[symDetails].value).toEqual(root.c);
-    expect(createTrackerProxy(root).c.l[symDetails].value).toEqual(root.c.l);
-    expect(createTrackerProxy(root).c.m[symDetails].value).toEqual(root.c.m);
-    expect(createTrackerProxy(root).d[symDetails].value).toEqual(root.d);
-    expect(createTrackerProxy(root).d[0][symDetails].value).toEqual(root.d[0]);
-    expect(createTrackerProxy(root).d[1][symDetails].value).toEqual(root.d[1]);
-    expect(createTrackerProxy(root).d[2][symDetails].value).toEqual(root.d[2]);
-    expect(createTrackerProxy(root).d[2].x[symDetails].value).toEqual(root.d[2].x);
-    expect(createTrackerProxy(root).d[2].y[symDetails].value).toEqual(root.d[2].y);
-    expect(createTrackerProxy(<any>root).e[symDetails].value).toBeUndefined();
+    expect(createTrackerProxy(root)[symTrackerDetails].value).toEqual(root);
+    expect(createTrackerProxy(root).a[symTrackerDetails].value).toEqual(root.a);
+    expect(createTrackerProxy(root).b[symTrackerDetails].value).toEqual(root.b);
+    expect(createTrackerProxy(root).c[symTrackerDetails].value).toEqual(root.c);
+    expect(createTrackerProxy(root).c.l[symTrackerDetails].value).toEqual(root.c.l);
+    expect(createTrackerProxy(root).c.m[symTrackerDetails].value).toEqual(root.c.m);
+    expect(createTrackerProxy(root).d[symTrackerDetails].value).toEqual(root.d);
+    expect(createTrackerProxy(root).d[0][symTrackerDetails].value).toEqual(root.d[0]);
+    expect(createTrackerProxy(root).d[1][symTrackerDetails].value).toEqual(root.d[1]);
+    expect(createTrackerProxy(root).d[2][symTrackerDetails].value).toEqual(root.d[2]);
+    expect(createTrackerProxy(root).d[2].x[symTrackerDetails].value).toEqual(root.d[2].x);
+    expect(createTrackerProxy(root).d[2].y[symTrackerDetails].value).toEqual(root.d[2].y);
+    expect(createTrackerProxy(<any>root).e[symTrackerDetails].value).toBeUndefined();
 });
 
 it('track parent', () => {
-    expect(createTrackerProxy(root)[symDetails].parent).toBeUndefined();
-    expect(createTrackerProxy(root).a[symDetails].parent).toEqual(root);
-    expect(createTrackerProxy(root).b[symDetails].parent).toEqual(root);
-    expect(createTrackerProxy(root).c[symDetails].parent).toEqual(root);
-    expect(createTrackerProxy(root).c.l[symDetails].parent).toEqual(root.c);
-    expect(createTrackerProxy(root).c.m[symDetails].parent).toEqual(root.c);
-    expect(createTrackerProxy(root).d[symDetails].parent).toEqual(root);
-    expect(createTrackerProxy(root).d[0][symDetails].parent).toEqual(root.d);
-    expect(createTrackerProxy(root).d[1][symDetails].parent).toEqual(root.d);
-    expect(createTrackerProxy(root).d[2][symDetails].parent).toEqual(root.d);
-    expect(createTrackerProxy(root).d[2].x[symDetails].parent).toEqual(root.d[2]);
-    expect(createTrackerProxy(root).d[2].y[symDetails].parent).toEqual(root.d[2]);
-    expect(createTrackerProxy(<any>root).e[symDetails].parent).toEqual(root);
+    expect(createTrackerProxy(root)[symTrackerDetails].parent).toBeUndefined();
+    expect(createTrackerProxy(root).a[symTrackerDetails].parent).toEqual(root);
+    expect(createTrackerProxy(root).b[symTrackerDetails].parent).toEqual(root);
+    expect(createTrackerProxy(root).c[symTrackerDetails].parent).toEqual(root);
+    expect(createTrackerProxy(root).c.l[symTrackerDetails].parent).toEqual(root.c);
+    expect(createTrackerProxy(root).c.m[symTrackerDetails].parent).toEqual(root.c);
+    expect(createTrackerProxy(root).d[symTrackerDetails].parent).toEqual(root);
+    expect(createTrackerProxy(root).d[0][symTrackerDetails].parent).toEqual(root.d);
+    expect(createTrackerProxy(root).d[1][symTrackerDetails].parent).toEqual(root.d);
+    expect(createTrackerProxy(root).d[2][symTrackerDetails].parent).toEqual(root.d);
+    expect(createTrackerProxy(root).d[2].x[symTrackerDetails].parent).toEqual(root.d[2]);
+    expect(createTrackerProxy(root).d[2].y[symTrackerDetails].parent).toEqual(root.d[2]);
+    expect(createTrackerProxy(<any>root).e[symTrackerDetails].parent).toEqual(root);
 });
 
 it('track property', () => {
-    expect(createTrackerProxy(root)[symDetails].property).toEqual(undefined);
-    expect(createTrackerProxy(root).a[symDetails].property).toEqual('a');
-    expect(createTrackerProxy(root).b[symDetails].property).toEqual('b');
-    expect(createTrackerProxy(root).c[symDetails].property).toEqual('c');
-    expect(createTrackerProxy(root).c.l[symDetails].property).toEqual('l');
-    expect(createTrackerProxy(root).c.m[symDetails].property).toEqual('m');
-    expect(createTrackerProxy(root).d[symDetails].property).toEqual('d');
-    expect(createTrackerProxy(root).d[0][symDetails].property).toEqual(0);
-    expect(createTrackerProxy(root).d[1][symDetails].property).toEqual(1);
-    expect(createTrackerProxy(root).d[2][symDetails].property).toEqual(2);
-    expect(createTrackerProxy(root).d[2].x[symDetails].property).toEqual('x');
-    expect(createTrackerProxy(root).d[2].y[symDetails].property).toEqual('y');
-    expect(createTrackerProxy(<any>root).e[symDetails].property).toEqual('e');
+    expect(createTrackerProxy(root)[symTrackerDetails].property).toEqual(undefined);
+    expect(createTrackerProxy(root).a[symTrackerDetails].property).toEqual('a');
+    expect(createTrackerProxy(root).b[symTrackerDetails].property).toEqual('b');
+    expect(createTrackerProxy(root).c[symTrackerDetails].property).toEqual('c');
+    expect(createTrackerProxy(root).c.l[symTrackerDetails].property).toEqual('l');
+    expect(createTrackerProxy(root).c.m[symTrackerDetails].property).toEqual('m');
+    expect(createTrackerProxy(root).d[symTrackerDetails].property).toEqual('d');
+    expect(createTrackerProxy(root).d[0][symTrackerDetails].property).toEqual(0);
+    expect(createTrackerProxy(root).d[1][symTrackerDetails].property).toEqual(1);
+    expect(createTrackerProxy(root).d[2][symTrackerDetails].property).toEqual(2);
+    expect(createTrackerProxy(root).d[2].x[symTrackerDetails].property).toEqual('x');
+    expect(createTrackerProxy(root).d[2].y[symTrackerDetails].property).toEqual('y');
+    expect(createTrackerProxy(<any>root).e[symTrackerDetails].property).toEqual('e');
 });
